c786235c22b55618c56efa490374ffb6b638c76a
ragnarlothbroke12 <siyaram490don@gmail.com>
2024-03-18 21:05:57 +0530

    Fotter component created and added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import SignUp from "./pages/SignUp"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
 import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
@@ -18,7 +19,9 @@ export default function App() {
       <Route path="/sign-up" element={<SignUp/>}/>
       <Route path="/dashboard" element={<Dashboard/>}/>
       <Route path="/projects" element={<Projects/>}/>
+
      </Routes>
+     <Footer/>
      </BrowserRouter>
   )
 }
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,10 +7,13 @@ export default function Header() {
     const path = useLocation().pathname;
   return (
     <Navbar className="border-b-2">
-       <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold
+       <Link 
+       to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold
         dark:text-white">
             <span className="px-2 py-1 bg-gradient-to-r from-indigo-500
-            via-purple-500 to-pink-500 rounded-lg text-white">Siyaram's</span>
+            via-purple-500 to-pink-500 rounded-lg text-white">
+                Siyaram's
+                </span>
             Blog
        </Link>
        <form>
