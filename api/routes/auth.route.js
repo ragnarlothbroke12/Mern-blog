474454d8970af3189942435dde6b391edbd81816
ragnarlothbroke12 <siyaram490don@gmail.com>
2024-03-19 22:40:40 +0530

    created signinn api

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { signup } from '../controlers/auth.controler.js';
+import { signin, signup } from '../controlers/auth.controler.js';
 
 const router = express.Router();
 
 
 router.post('/signup',signup);
+router.post('/signin',signin);
 
 export default router;
