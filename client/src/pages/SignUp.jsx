774971ab48a46bc6b47b1ed5a3da099fc8b1f067
ragnarlothbroke12 <siyaram490don@gmail.com>
2024-03-20 19:51:38 +0530

    redux toolkit added

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,11 +1,13 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate} from "react-router-dom";
+import { useDispatch,useSelector} from "react-redux";
+import { signInStart,signInSuccess,signInFailure } from "../redux/user/userSlice";
 
 export default function SignUp() {
   const [formData, setFormData] = useState({});
-  const [errorMessage,setErrorMessage] = useState(null);
-  const [loading,setLoading] = useState(false);
+  const {loading, error:errorMessage}= useSelector(state=> state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const handleChange = (e)=>{
     setFormData({...formData,[e.target.id]:e.target.value.trim()});
@@ -13,11 +15,10 @@ export default function SignUp() {
  const handleSubmit = async (e)=> {
     e.preventDefault();
     if(!formData.username || !formData.email || !formData.password){
-      return setErrorMessage('Please fill out all field.');
+      return dispatch(signInFailure('Please fill out all field.'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart());
       const res = await fetch('/api/auth/signup',{
         method: 'POST',
         headers:{'Content-Type':'application/json'},
@@ -25,15 +26,14 @@ export default function SignUp() {
       });
     const data = await res.json();
     if(data.success === false){
-      return setErrorMessage(data.message);
+      dispatch(signInFailure(data.message));
     }
-    setLoading(false);
-    if (res.ok){
-      navigate('/sign-in');
+    if(res.ok){
+      dispatch(signInSuccess(data));
+      navigate('/');
     }
    } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
  };
   return <div className="min-h-screen mt-20">
