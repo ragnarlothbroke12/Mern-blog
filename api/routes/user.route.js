b7179cf1391b0b3c1d38636be8da5036ab58e6b3
ragnarlothbroke12 <siyaram490don@gmail.com>
2024-03-29 15:52:25 +0530

    added update user api router

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,10 @@
 import express from 'express';
-import { test } from '../controlers/user.controler.js';
+import { test, updateUser } from '../controlers/user.controler.js';
+import { verifyToken } from '../utlis/verifyUser.js';
 
 const router = express.Router();
 
 router.get('/test',test);
+router.put('/update/:userId',verifyToken ,updateUser);
 
 export default router;
