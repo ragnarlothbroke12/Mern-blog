926bf424ea58ecaecfcc1e9d810733d5c3ea7910
ragnarlothbroke12 <siyaram490don@gmail.com>
2024-04-25 12:48:54 +0530

    added signout functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -49,6 +49,11 @@ const userSlice = createSlice({
             state.loading = false;
             state.error = action.payload;
         },
+        signoutSuccess: (state) => {
+            state.currentUser = null;
+            state.error = null;
+            state.loading = false;
+        }
 
     },
 });
@@ -62,7 +67,8 @@ export const {
     updateFailure,
     deleteUserStart,
     deleteUserSucess,
-    deleteUserFailure
+    deleteUserFailure,
+    signoutSuccess
 } = userSlice.actions;
 
 export default userSlice.reducer;
