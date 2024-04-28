dc99a4b41af71f48869de6c76898f7144b690228
ragnarlothbroke12 <siyaram490don@gmail.com>
2024-04-28 13:41:50 +0530

    add update post functionality

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,12 +1,14 @@
 import express from 'express';
 import {verifyToken} from '../utlis/verifyUser.js';
-import { create, deletepost, getposts } from '../controlers/post.controller.js';
+import { create, deletepost, getposts, updatepost } from '../controlers/post.controller.js';
+
 
 const router = express.Router();
 
 router.post('/create',verifyToken, create)
 router.get('/getposts',getposts)
 router.delete('/deletepost/:postId/:userId',verifyToken,deletepost);
+router.put('/updatepost/:postId/:userId', verifyToken, updatepost)
 
 
 export default router;
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,7 @@ import Footer from "./components/Footer"
 import PrivateRoute from "./components/PrivateRoute"
 import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute"
 import Createpost from "./pages/Createpost"
+import Updatepost from "./pages/UpdatePost"
 
 export default function App() {
   return (
@@ -25,6 +26,7 @@ export default function App() {
       </Route>
       <Route element = {<OnlyAdminPrivateRoute />} >
       <Route path="/create-post" element={<Createpost/>}/>
+      <Route path="/update-post/:postId" element={<Updatepost/>}/>
       </Route>
       <Route path="/projects" element={<Projects/>}/>
      </Routes>
